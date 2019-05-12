ae5251d8ce05f8d0a20679f1d6d80b75a7923e37
Carl Pearson <pearson@illinois.edu>
2019-05-12 07:32:55 -0500

    switch to CUDA 10.0

--- src/main.cu
+++ src/main.cu
@@ -1,16 +1,19 @@
 #include <iostream>
 
+inline void checkCuda(cudaError_t result, const char *file, const int line) {
+    if (result != cudaSuccess) {
+      std::cerr << file << "@" << line << ": CUDA Runtime Error: " << cudaGetErrorString(result) << std::endl;
+      exit(-1);
+    }
+  }
 
+  #define CUDA_RUNTIME(stmt) checkCuda(stmt, __FILE__, __LINE__);
 
 int main(void) {
-    std::cout << "launched main\n";
+    std::cout << "enter main\n";
 
     cudaDeviceProp prop;
-    cudaError_t ret = cudaGetDeviceProperties(&prop, 0);
-    if (ret != cudaSuccess) {
-        std::cout << "error\n";
-        return 1;
-    }
+    CUDA_RUNTIME(cudaGetDeviceProperties(&prop, 0))
 
     std::cout << &prop << "\n";
 
