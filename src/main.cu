79457c0ba25edd7b78e11ef49dcc1f003e92ad76
Carl Pearson <pearson@illinois.edu>
2019-05-12 07:39:13 -0500

    add simple test kernel

--- src/main.cu
+++ src/main.cu
@@ -2,12 +2,19 @@
 
 inline void checkCuda(cudaError_t result, const char *file, const int line) {
     if (result != cudaSuccess) {
-      std::cerr << file << "@" << line << ": CUDA Runtime Error: " << cudaGetErrorString(result) << std::endl;
-      exit(-1);
+        std::cerr << file << "@" << line << ": CUDA Runtime Error: " << cudaGetErrorString(result) << std::endl;
+        exit(-1);
     }
-  }
+}
 
-  #define CUDA_RUNTIME(stmt) checkCuda(stmt, __FILE__, __LINE__);
+#define CUDA_RUNTIME(stmt) checkCuda(stmt, __FILE__, __LINE__);
+
+template <typename T>
+__global__ void setter_kernel(T *x, const size_t n) {
+    for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += gridDim.x * blockDim.x) {
+        x[i] = 10;
+    }
+}
 
 int main(void) {
     std::cout << "enter main\n";
@@ -15,7 +22,20 @@ int main(void) {
     cudaDeviceProp prop;
     CUDA_RUNTIME(cudaGetDeviceProperties(&prop, 0))
 
-    std::cout << &prop << "\n";
+    std::cout << prop.name << "\n";
 
+    float *x;
+    CUDA_RUNTIME(cudaMallocManaged(&x, sizeof(*x) * 10));
+    setter_kernel<<<10, 512>>>(x, 10);
+    CUDA_RUNTIME(cudaDeviceSynchronize());
+
+    if (x[0] != 10) {
+        std::cerr << "setter kernel failed\n";
+        return 1;
+    }
+
+    CUDA_RUNTIME(cudaFree(x));
+
+    std::cerr << "tests passed\n";
     return 0;
 }
